2972a49c728b5a9feb21d2595903837e97d081f1
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-29 12:28:50 +0200

    Snack 3

---
+++ Snack 3/index.php
@@ -0,0 +1,74 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+// $keys = array_keys($posts);
+// echo "<h2>{$keys[$i];}</h2>";
+
+// for($i = 0; $i < count($keys); $i++){
+
+//     var_dump($posts[$keys[$i]]);
+// }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <?php
+            foreach($posts as $key => $value){
+                echo "<h2>{$key}</h2> <ul>";
+
+                foreach ($value as $props => $item) {
+                    echo "<li>{$item['title']} - {$item['author']} - {$item['text']}</li>";
+                }
+                echo "</ul>";
+            }
+        ?>
+    </main>
+    
+</body>
+</html>
