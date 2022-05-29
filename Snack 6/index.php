d2f7009aa570ec83a81f5b788fdc27ef63303290
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-29 15:06:26 +0200

    snack 6

---
+++ Snack 6/index.php
@@ -0,0 +1,48 @@
+
+<?php
+ 
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+            foreach($db as $key => $value){
+                echo "<h2>{$key}</h2> ";
+
+            //     foreach ($value as $props => $item) {
+            //         echo "<li>{$item['title']} - {$item['author']} - {$item['text']}</li>";
+            //     }
+            //     echo "</ul>";
+            }
+        ?>
+</body>
+</html>
