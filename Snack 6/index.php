df85e1ced29a2f2a8bbd2ce5890e46dcd862fc8e
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-30 16:50:23 +0200

    index6 stampa

--- Snack 6/index.php
+++ Snack 6/index.php
@@ -31,18 +31,26 @@ $db = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
 <?php
             foreach($db as $key => $value){
                 echo "<h2>{$key}</h2> ";
-
+                
+               
                foreach ($value as $props => $item) {
-                    echo "{$item['name']} - {$item['lastname']} ";
+                if($item == 'teachers'){
+                    echo "<p class='grigio'>{$item['name']} - {$item['lastname']}</p>"; 
+                   }else{
+                        echo "<p class='verde'>{$item['name']} - {$item['lastname']}</p>";
+                   }
                }
                 
             }
         ?>
+
+        
 </body>
 </html>
